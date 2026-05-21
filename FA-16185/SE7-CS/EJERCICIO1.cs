7623356460a7390ce81f997a813b763b3210a7f4
max <lobo.wolft2022@gmail.com>
2026-05-21 15:54:51 -0500

    actualizando la semana8

---
+++ FA-16185/SE7-CS/EJERCICIO1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE7_CS
+{
+    internal class EJERCICIO1
+    {
+    }
+}
